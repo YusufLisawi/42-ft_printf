06b3841ee78ae855b63c3806124b2b98a666e0c3
Youssef Elaissaoui <yelaissa@c1r2p7.1337.ma>
2022-10-19 10:31:54 +0100

    ft_printf v1

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelaissa <yelaissa@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 10:25:35 by yelaissa          #+#    #+#             */
+/*   Updated: 2022/10/19 10:27:35 by yelaissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+
+# include "ft_printf_utils/utils.h"
+
+int	ft_printf(const char *s, ...);
+
+#endif
